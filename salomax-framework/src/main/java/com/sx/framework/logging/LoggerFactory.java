881f6d6f3b1ef26400e07ed8dd42287ce2f191aa
salomao.marcos <salomao.marcos@gmail.com>
2013-10-29 16:21:45 -0200

    Logger

---
+++ salomax-framework/src/main/java/com/sx/framework/logging/LoggerFactory.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2013.
+ *  
+ *  Este arquivo é parte do programa SALOMAX FRAMEWORK.
+ *  NÃO REMOVA OU MODIFIQUE o cabeçalho deste arquivo contendo sua licença de uso.
+ *  
+ * SALOMAX FRAMEWORK é um software livre; você pode redistribuí-lo e/ou 
+ * modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+ * publicada pela Fundação do Software Livre (FSF); na versão 2 da 
+ * Licença, ou (na sua opinião) qualquer versão.
+
+ * Este programa é distribuído na esperança de que possa ser  útil, 
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
+ * MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ * Licença Pública Geral GNU para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa, se não, escreva para a Fundação do Software
+ * Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.sx.framework.logging;
+
+import java.util.logging.Logger;
+
+/**
+ * Factory pattern to Logger (java.util.logging) class.
+ * 
+ * @author marcos.salomao
+ */
+public class LoggerFactory {
+	
+	/**
+	 * Factory logger to java.util.logging package.
+	 * 
+	 * @param class1 Class to logging
+	 * @return A java.util.logging.Logger instance
+	 */
+	public static <T> Logger getLogger(Class<T> class1) {
+		// TODO Use spring 
+		return Logger.getLogger(class1.getCanonicalName());
+	}
+
+}
